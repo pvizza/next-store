6ac40a4b793570bcaa767016684ab11a2c675779
PB <ubuntu@DESKTOP-O3L82RA.localdomain>
2021-12-10 09:22:30 -0300

    add apollo config

--- Frontend/pages/_app.tsx
+++ Frontend/pages/_app.tsx
@@ -2,16 +2,19 @@ import '../styles/globals.css'
 import "nprogress/nprogress.css";
 import type { AppProps } from 'next/app'
 import Main from '../components/main/main'
+import {ApolloProvider} from '@apollo/client'
+import configApollo from '../configApollo'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps,apollo }: AppProps) {
   return (
   
+      <ApolloProvider client={apollo}>
       <Main>
       <Component {...pageProps} />
       </Main>
-  
+      </ApolloProvider>
   )
   
 }
 
-export default MyApp
+export default configApollo(MyApp)
